64ac608845ed04cb173cec01ceaf8f3573dcaea4
Denis Caminha <denis@MacBook-Air-de-Denis.local>
2023-05-18 18:18:42 -0300

    fixing jpa annotations

--- src/main/kotlin/br/com/alura/forum/model/Resposta.kt
+++ src/main/kotlin/br/com/alura/forum/model/Resposta.kt
@@ -6,6 +6,7 @@ import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.ManyToOne
 
 @Entity
 data class Resposta(
@@ -13,7 +14,9 @@ data class Resposta(
         val id: Long? = null,
         val mensagem: String,
         val dataCriacao: LocalDateTime = LocalDateTime.now(),
+        @ManyToOne
         val autor: Usuario,
+        @ManyToOne
         val topico: Topico,
         val solucao: Boolean
 )
