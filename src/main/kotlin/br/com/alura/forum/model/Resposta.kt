32898a2108eeabe9b819baad8b4d08ad0094441d
Denis Caminha <denis@MacBook-Air-de-Denis.local>
2023-05-18 18:14:13 -0300

    add jpa

--- src/main/kotlin/br/com/alura/forum/model/Resposta.kt
+++ src/main/kotlin/br/com/alura/forum/model/Resposta.kt
@@ -2,8 +2,14 @@ package br.com.alura.forum.model
 
 import java.time.LocalDate
 import java.time.LocalDateTime
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
 
+@Entity
 data class Resposta(
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
         val id: Long? = null,
         val mensagem: String,
         val dataCriacao: LocalDateTime = LocalDateTime.now(),
