13d2c86b9469a3ddbe3145662dd21086965df0a0
Denis Caminha <denis@MacBook-Air-de-Denis.local>
2023-05-19 08:13:54 -0300

    changing code in service to jpa way

--- src/main/kotlin/br/com/alura/forum/service/CursoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/CursoService.kt
@@ -1,26 +1,13 @@
 package br.com.alura.forum.service
 
 import br.com.alura.forum.model.Curso
+import br.com.alura.forum.repository.CursoRepository
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class CursoService(var cursos: List<Curso>) {
-
-    init {
-        val curso = Curso(
-                id = 1,
-                nome = "Kotlin",
-                categoria = "Programacao"
-        )
-        cursos = Arrays.asList(curso)
-    }
-
-    fun buscarPorId(id: Long): Curso {
-        return cursos.stream().filter({
-            c -> c.id == id
-        }).findFirst().get()
-    }
-
-
+class CursoService(
+    private val cursoRepository: CursoRepository
+) {
+    fun buscarPorId(id: Long): Curso = cursoRepository.getOne(id)
 }
